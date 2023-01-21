0a634a0f672236db81e5257c070bd6e673e38b8f
mason-nz <70310695+mason-nz@users.noreply.github.com>
2023-01-21 22:51:28 +1300

    Add files via upload
    Add ReadyTech Developer Technical Test (Normal Part)

---
+++ ReadyTech.BrewCoffee2023.API/Controllers/BrewCoffeeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ReadyTech.BrewCoffee2023.API.Controllers
+{
+    [Route("brew-coffee")]
+    public class BrewCoffeeController : Controller
+    {
+        private static int _callCount = 0;
+        private const int _callLimit = 5;
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            _callCount++; //add call count 1
+
+            var currentLocalDateTime = DateTime.Now;
+
+            // if today was 1st April
+            if (currentLocalDateTime.Day == 1 && currentLocalDateTime.Month == 4)
+            {
+                return StatusCode(StatusCodes.Status418ImATeapot, "");
+            }
+
+            // if called count == 5 times
+            if (_callCount >= _callLimit)
+            {
+                _callCount = 0;
+                return StatusCode(StatusCodes.Status503ServiceUnavailable, "");
+            }
+
+            dynamic result = new ExpandoObject();
+            result.message = "Your piping hot coffee is ready";
+            //result.callCount = _callCount;
+            /*
+             * format: zzz +13:00
+             * format: zz  +13
+             */
+            result.prepared = currentLocalDateTime.ToString("yyyy-MM-ddTHH:mm:sszz00");
+            return Json(result);
+        }
+    }
+}
+
---
+++ ReadyTech.BrewCoffee2023.API/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//Add a middleware to handle the exception.If the URL doesn't match the actions of the controllers, will go to the specific Error action.
+app.UseStatusCodePagesWithReExecute("/api/error/{0}");
+
+//Add an ExceptionHandler for the middleware.
+app.UseExceptionHandler(errorApp =>
+{
+    errorApp.Run(async context =>
+    {
+        context.Response.StatusCode = 200;
+        context.Response.ContentType = "application/json; charset=utf-8";
+
+        var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerFeature>();
+        await context.Response.WriteAsync(text: exceptionHandlerPathFeature?.Error.Message);
+    });
+});
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
